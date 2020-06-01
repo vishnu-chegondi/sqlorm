2e290c1576f8cfaf0d1efb417b317c7f1ff3faad
Vishnu Prasad <vishnu.chegondi@gmail.com>
2020-06-01 21:16:50 +0530

    Added Data Definition Queries (#3)
    * Add examples to config and fields
    
    * Create go.yml
    
    * Update README
    
    * Added Create and Drop Table with Examples

--- fields_test.go
+++ fields_test.go
@@ -3,7 +3,7 @@ package sqlorm
 import "fmt"
 
 func ExampleField() {
-	field := NewField("test", VarChar, "TestValue")
+	field := NewField("test", VarChar, "TestValue", false, 0)
 	if field.ValidType() && field.ValidDefaultValue() {
 		fmt.Println("Successfully created field")
 	}
--- statements/statements.go
+++ statements/statements.go
@@ -1,13 +1,21 @@
 package statements
 
 import (
-	"database/sql"
 	"fmt"
 )
 
 /*
-CreateTableStmnt is used for generating a statement for creating a table of given database
+CreateTableStmnt is used for generating a statment for creating a table of given database
 */
-func CreateTableStmnt(db *sql.DB, database string) {
-	fmt.Print("Test")
+func CreateTableStmnt(tableName string, driverName string) string {
+	query := fmt.Sprintf("CREATE TABLE %s (", tableName)
+	return query
+}
+
+/*
+DropTableStmnt is used for generating a statment for droping a table in given database
+*/
+func DropTableStmnt(tableName string, driverName string) string {
+	query:= fmt.Sprintf("DROP TABLE %s", tableName)
+	return query
 }
