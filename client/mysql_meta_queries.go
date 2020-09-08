a33b9bda4299b8eacf561327c82b1b1ed0df8ca8
Vishnu Prasad <vishnu.chegondi@gmail.com>
2020-09-08 19:17:33 +0530

    Feature/sqlor 1 vishnu create package structure (#10)
    * final create structure
    
    * Update Table Check
    
    * update base structure
    
    Co-authored-by: vishnu-chegondi <vishnu.chegondi@fissionlabs.com>

--- client/mysql_meta_queries.go
+++ client/mysql_meta_queries.go
@@ -4,78 +4,77 @@ import (
 	"context"
 	"database/sql"
 	"os"
+	"sync"
 
 	mysql_connector "github.com/codeamenity/mysql_ormdriver/connector"
 	mysql_statements "github.com/codeamenity/mysql_ormdriver/statements"
 )
 
 type Client interface {
-	getDb() (*sql.DB, error)
+	setDb()
 	Ping() error
-	GetAllTables() (*sql.Rows, error)
-	GetTableColumns() (*sql.Rows, error)
+	GetTable(ctx context.Context, tableName string) (*sql.Rows, error)
+	GetTableColumns(ctx context.Context, tableName string) (*sql.Rows, error)
 }
 
 type MYSQL struct {
 	User   string // Username
 	Passwd string // Password (requires User)
-	Net    string // Network type
+	Net    string `default:"tcp"` // Network type
 	Addr   string // Network address (requires Net)
 	DBName string // Database name
-	db     *sql.DB
+	Db     *sql.DB
 }
 
-func (db_driver *MYSQL) getDb() error {
-	var err error
-	if db_driver.User != "" {
-		os.Setenv("User", db_driver.User)
-	}
-	if db_driver.Passwd != "" {
-		os.Setenv("Passwd", db_driver.Passwd)
-	}
-	if db_driver.Net != "" {
-		os.Setenv("Net", db_driver.Net)
-	}
-	if db_driver.Addr != "" {
-		os.Setenv("Addr", db_driver.Addr)
-	}
-	if db_driver.DBName != "" {
-		os.Setenv("DBName", db_driver.DBName)
-	}
-	if err = db_driver.db.Ping(); err != nil {
-		db, err := mysql_connector.GetDb()
-		if err == nil {
-			db_driver.db = db
+func (db_driver *MYSQL) setDb() {
+	var once sync.Once
+	once.Do(func() {
+		if db_driver.User != "" {
+			os.Setenv("User", db_driver.User)
 		}
-	}
-	return err
+		if db_driver.Passwd != "" {
+			os.Setenv("Passwd", db_driver.Passwd)
+		}
+		if db_driver.Net != "" {
+			os.Setenv("Net", db_driver.Net)
+		}
+		if db_driver.Addr != "" {
+			os.Setenv("Addr", db_driver.Addr)
+		}
+		if db_driver.DBName != "" {
+			os.Setenv("DBName", db_driver.DBName)
+		}
+		db, err := mysql_connector.GetDb()
+		if err != nil {
+			panic(err.Error())
+		}
+		db_driver.Db = db
+	})
 }
 
 func (db_driver *MYSQL) Ping() error {
-	err := db_driver.getDb()
-	if err == nil {
-		err = db_driver.db.Ping()
-	}
+	db_driver.setDb()
+	err := db_driver.Db.Ping()
 	return err
 }
 
-func (db_driver *MYSQL) GetAllTables(ctx context.Context, DBName string) (*sql.Rows, error) {
+func (db_driver *MYSQL) GetTable(ctx context.Context, tableName string) (*sql.Rows, error) {
+	db_driver.setDb()
 	var rows *sql.Rows
-	err := db_driver.getDb()
-	stmt, err := mysql_statements.GetAllTablesStmnt(ctx, db_driver.db)
+	stmt, err := mysql_statements.GetTableStmnt(ctx, db_driver.Db)
 	if err == nil {
-		rows, err = stmt.QueryContext(ctx, DBName)
+		rows, err = stmt.QueryContext(ctx, db_driver.DBName, tableName)
 		defer stmt.Close()
 	}
 	return rows, err
 }
 
-func (db_driver *MYSQL) GetTableColumns(ctx context.Context, DBName, tableName string) (*sql.Rows, error) {
+func (db_driver *MYSQL) GetTableColumns(ctx context.Context, tableName string) (*sql.Rows, error) {
+	db_driver.setDb()
 	var rows *sql.Rows
-	err := db_driver.getDb()
-	stmt, err := mysql_statements.GetColumnsOfTable(ctx, db_driver.db)
+	stmt, err := mysql_statements.GetColumnsOfTable(ctx, db_driver.Db)
 	if err == nil {
-		rows, err = stmt.QueryContext(ctx, DBName, tableName)
+		rows, err = stmt.QueryContext(ctx, db_driver.DBName, tableName)
 		defer stmt.Close()
 	}
 	return rows, err
